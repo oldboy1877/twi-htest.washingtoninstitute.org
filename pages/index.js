17f542040625d8737d36699ac118a993f1499919
oldboy1877 <105782772+oldboy1877@users.noreply.github.com>
2022-05-18 02:59:14 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,160 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <html><head>
+<meta property="og:image" content="https://i.imgur.com/kLyjjOg.jpg">
+<link rel="icon" href="https://i.imgur.com/PFxAtIi.png">
+<meta content='H4cked by Overthinker1877, Overthinker1877 , Overthinker 1877, 1877 Team, 1877 hack, H4cked by 1877' name='description' />
+<meta content='H4cked By Overthinker 1877, 1877 Team, Kurdish Hackers, Kurdish Hacker' name='keywords' />
+<meta content='H4cked By Overthinker 1877, 1877 Team, Kurdish Hackers, Kurdish Hacker' name='Abstract' />
+<meta name="title" content="H4cked by Overthinker1877 | 1877 Team | Kurdish Hacker">
+<meta name="description" content="Overthinker 1877 was here!">
+<meta name="keywords" content="Overthinker 1877">
+<meta name="googlebot" content="index,follow" />
+<meta name="robots" content="all" />
+<meta name="robots schedule" content="auto" />
+<meta name="distribution" content="global" />
+<title>H4cked by 1877 Team & ALtahrea Team</title>
+<meta name="robots" content="all" /><meta name="robots schedule" content="auto" />
+<meta name="distribution" content="global" /><link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono|Rajdhani|Oswald:700|Iceland|PT+Sans&display=swap" rel="stylesheet" type="text/css">
+<base target='_blank' /><body><center>
+<meta content='width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;' name='viewport' />
+<meta name="viewport" content="width=device-width" />
+<style>
+	*{font-family: Iceland, sans-serif !important;}
+</style>
+<style>
+.gambargetar{position:relative}
+img:hover{opacity:1}textarea{resize:none}
+img {
+    position:relative;opacity:1.0
+}
+</style>
+<style type="text/css">
+@media only screen and (max-device-width: 480px) {
+html, body {
+  overflow-x: hidden;
+}
+body {
+  position: relative;
+}
+    img {    
+    weight: 100%;
+    position: relative;
+    margin: -4;
+}
+</style>
+<style type="text/css">body, a:hover {cursor: url(https://cur.cursors-4u.net/cursors/cur-7/cur686.cur), progress !important;}</style>
+</script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style type="text/css">
+  #mouse-circle {
+    position: absolute;
+    width: 32px;
+    height: 32px;
+    margin: -16px 0 0 -16px;
+    border: 1px solid yellow;
+    border-radius: 50%;
+    pointer-events: none;
+}
+   body,td,th {
+   color: #000;
+   margin: 0;
+   padding: 0;
+   height: 100%;
+   position: relative;
+   font-family: 1877;
+   background-color:#000000;
+   }
+</style>
+<body oncontextmenu="return false;">
+</body>
+<body oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;'>
+<FONT size="3" color=#FFFF00><CENTER><BIG>&#9733; <FONT size="3" color=white><BIG>&#9733;</BIG></FONT> &#9733; <FONT size="3" color=white><BIG>&#9733;</BIG></FONT> &#9733; <FONT size="3" color=white><BIG>&#9733;</BIG></FONT> &#9733;</BIG></CENTER></FONT>
+<div style="line-height:40%;">
+<br>
+</div>
+<div id="mouse-circle"></div>
+<img src="https://i.imgur.com/kLyjjOg.jpg" width="350" style="border-right: 2px white solid;border-left: 2px yellow solid;border-top: 2px yellow solid;border-bottom: 2px white solid;object-fit: scale-down; max-width: 100%; border-radius: 0%;" onclick="location.href = 'https://t.me/x1877x';">
+<div style="line-height:50%;">
+<br>
+</div>
+<a <div id="center"> <font color="white" size="7" face="Share Tech Mono">H4cked by <font color=#FFFF00> </font> <a style="text-decoration:none" href="https://t.me/x1877x"> <font color=#FFFF00> 1877 & ALtahrea Team</a></font>
+<div style="line-height:30%;">
+<br>
+</div>
+<div id="center"> <font color="white" size="6" face="Share Tech Mono">We ll never forgive<br>We ll never forget<br>Fuck USA Fuck NATO<br>يالثارات قاسم سليماني</a></div>
+<div style="line-height:30%;">
+</div>
+<FONT color=#FF0000></font>
+</body>
+</center>
+<!--<body onclick="popup()">-->
+<style type="text/css">
+.xxoverthinker1877{ 
+background:transparent;
+border:3px solid white;
+text-shadow:0px 0px 5px yellow;
+border-radius:13px; font-family:Share Tech Mono;
+color:yellow; font-size:18x; font-weight:bold; padding:3px 29px; text-decoration:none; text-shadow:0px 0px 20px #dbe600;height:40px;width:110px; } </style>
+<center>
+<div style="line-height:70%;">
+<br>
+</div>
+<audio id="xxoverthinker1877" src="https://b.top4top.io/m_2329nm5cb1.mp3"></audio>
+</audio>
+<button class="xxoverthinker1877" onclick="play();x1877x();"><font size="4" color="White">PLAY</font></button> <button class="xxoverthinker1877" onclick="pause(); x1877x();"><font size="4" color="white">PAUSE</font></button> <audio id="xxoverthinker1877" src="https://b.top4top.io/m_2329nm5cb1.mp3"></audio> </audio>
+<div style="line-height:50%;">
+<br>
+</div>
+<marquee width="450" scrollamount="12" behavior="scroll" direction="left"><font color="yellow" size="6" face="Share Tech Mono">1877 Team</font><font color="white" size="6" face="Share Tech Mono"> & ALtahrea Team</font></marquee>
+<div style="line-height:30%;">
+<br>
+</div>
+<font color="white" size="5" face="Share Tech Mono">-=-=-=-=-=-=-=-=-</font>
+</center>
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+    let mousePosX = 0,
+        mousePosY = 0,
+        mouseCircle = document.getElementById('mouse-circle');
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+    document.onmousemove = (e) => {
+        mousePosX = e.pageX;
+        mousePosY = e.pageY;
+    }
 
-      <Footer />
-    </div>
+    let delay = 8,
+        revisedMousePosX = 0,
+        revisedMousePosY = 0;
+
+    function delayMouseFollow() {
+        requestAnimationFrame(delayMouseFollow);
+
+        revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
+        revisedMousePosY += (mousePosY - revisedMousePosY) / delay; 
+
+        mouseCircle.style.top = revisedMousePosY + 'px';
+        mouseCircle.style.left = revisedMousePosX + 'px';
+    }
+    delayMouseFollow();
+});
+    document.addEventListener("contextmenu", function(e){
+        e.preventDefault();
+    }, false);
+    </script>
+    <script>
+  document.onkeydown = function (e) {
+          return false;
+  }
+  </script>
+  <script type="text/javascript">
+    var popup = function() {
+        window.open ("https://t.me/x1877x", "Window","status=1,toolbar=1");
+        window.open ("https://1877.to/", "Window2","status=1,toolbar=1");
+    }
+</script>
+<script> function play(){ var audio = document.getElementById('xxoverthinker1877'); audio.play(); } function x1877x(){ document.getElementById('xxoverthinker1877xx').style.visibility='visible'; } function pause(){ var audio = document.getElementById('xxoverthinker1877'); audio.pause(); } function x1877x(){ document.getElementById('xxoverthinker1877xx').style.visibility='visible'; }</script>
+</html>
   )
 }
